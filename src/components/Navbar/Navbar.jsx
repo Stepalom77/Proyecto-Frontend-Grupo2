455a5115a0a17af431b267e7b2f4af8b6f06af99
Stepalom77 <stepalom22@gmail.com>
2023-05-18 17:38:49 -0500

    Navbar css y html echo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,43 @@
 import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDays, faArrowRightToBracket, faPaw } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg bg-light mb-2">
-        <div className="container-fluid">
-            <button className="navbar-brand border border-0 bg-transparent" > three pics</button>
-            <button className="btn border border-0" type="button" >
-    
-            </button>
-        </div>
-    </nav>
+        <nav className="navbar navbar-expand-lg mb-2 " id='navbar'>
+            <div className="container-fluid">
+                <p className="navbar-brand border border-0 bg-transparent ms-2 " id='navbar-brand'> MAPet</p>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse ms-4" id="navbarNav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item" id='nav-item'>
+                            <button className="btn border border-0" type="button" id='navbar-button'>
+                                <span>Home</span>
+                            </button>
+                        </li>
+                        <li className="nav-item" id='nav-item'>
+                            <button className="btn border border-0" type="button" id='navbar-button'>
+                                <span>Lost Pets</span> <FontAwesomeIcon icon={faCalendarDays} id='icon'/>
+                            </button>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav" id='navbar-left'>
+                        <li className="nav-item" id='nav-item'>
+                            <button className="btn border border-0" type="button" id='navbar-button'>
+                                <span >Login</span> <FontAwesomeIcon icon={faArrowRightToBracket} id='icon'/>
+                            </button>
+                        </li>
+                        <li className="nav-item" id='nav-item'>
+                            <button className="btn border border-0" type="button" id='navbar-button'>
+                                <span >Register</span> <FontAwesomeIcon icon={faPaw} id='icon'/>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
