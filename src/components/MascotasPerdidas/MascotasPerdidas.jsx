00103e25842f6a1318e05939bca0abe7212010e4
Stepalom77 <stepalom22@gmail.com>
2023-10-24 22:23:05 -0500

    Cambios de MascotasPerdidas

--- src/components/MascotasPerdidas/MascotasPerdidas.jsx
+++ src/components/MascotasPerdidas/MascotasPerdidas.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState, useContext } from 'react';
 import './MascotasPerdidas.css';
 import { MyContext } from '../../context/MyProvider';
 import { API_ROUTE } from '../../helpers/ApiRoute';
+import { useNavigate } from "react-router-dom";
 
 const MascotasPerdidas = () => {
+    const navigate = useNavigate();
     const [lostPets, setLostPets] = useState([]);
     const { currentUser, isLogin } = useContext(MyContext);
 
@@ -15,9 +17,9 @@ const MascotasPerdidas = () => {
 
         const fetchLostPets = async () => {
             try {
-                const response = await fetch( API_ROUTE + 'pets', { headers });
+                const response = await fetch( API_ROUTE + 'pets/status/2', { headers });
                 const data = await response.json();
-                const lostPetsData = data.filter(pet => pet.status_id === 2);
+                const lostPetsData = data;
                 setLostPets(lostPetsData);
             } catch (error) {
                 console.error('Error fetching lost pets:', error);
@@ -26,6 +28,10 @@ const MascotasPerdidas = () => {
 
         fetchLostPets();
     }, []);
+
+    const toPet = (petId) => {
+        navigate(`/pet/${petId}`)
+    }
     return(
         <div className="">
             {/* Banner */}
@@ -55,24 +61,22 @@ const MascotasPerdidas = () => {
                             <div class="row justify-content-around bounce-in-bottom" id="cardDisplayer">
                                 {/* Ejemplo */}
                                 {lostPets.map(pet => (
-                                    <article class="card col-md-10 col-lg-5 col-xl-4" key={pet.pet_id}>
+                                    <article class="card col-md-10 col-lg-5 col-xl-4"  key={pet.pet_id} onClick={() => toPet(pet.pet_id)}>
                                             
                                         {/*<a className="pet-name" style={{textDecoration: 'none !important', color: '#000A36 !important'}} href="/pet/123">
                                             <img src="https://media.discordapp.net/attachments/1063874117507494048/1072561078661300224/abuela_de_dos_floppy_eared_Beauceron_dog_happy_tongue_out_using_e4270418-f93d-403b-9487-07c1dfb275dd.png?width=1024&height=1024" class="card-img-top" alt="" />
                                         </a>*/}
 
-                                        {pet.photos[0]?.url && <a className="pet-name" style={{textDecoration: 'none !important', color: '#000A36 !important'}} href='#'>
-                                            <img src={pet.photos[0]?.url} class="card-img-top" alt={pet.name} />
-                                        </a> }
+                                        {pet.photos[0]?.url && <img src={pet.photos[0]?.url} class="card-img-top" alt={pet.name} />}
                                         
                                         <div className="card-body">
                                             <h5 className="card-title">{pet.name}</h5>
                                             <p className="card-text special"><span className="special" >Type: </span>{pet.type}</p>
                                             <p className="card-text special"><span className="special" >Breed: </span>{pet.breed}</p>
                                             <p className="card-text special"><span className="special" >Age: </span>{pet.age}</p>
-                                            {/*isLogin && <p className="card-text special"><span className="special" >Owner: </span>{pet.owner.username}</p>*/}
-                                            {/*isLogin && <p className="card-text contact"><span className="material-symbols-outlined" >call</span> {pet.owner.mobile_number} </p>*/}
-                                            {/*isLogin && <p className="card-text contact"><span className="material-symbols-outlined" >mail</span> {pet.owner.email} </p>*/}
+                                            {isLogin && <p className="card-text special"><span className="special" >Owner: </span>{pet.owner.username}</p>}
+                                            {isLogin && <p className="card-text contact"><span className="material-symbols-outlined" >call</span> {pet.owner.mobile_number} </p>}
+                                            {isLogin && <p className="card-text contact"><span className="material-symbols-outlined" >mail</span> {pet.owner.email} </p>}
                                             <div className="btns-wrapper">
                                             {currentUser && (
                                                 <button className="btn btn-mapet-primary">Contact owner <span className="material-symbols-outlined">pets</span></button>
